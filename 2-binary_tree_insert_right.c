74e4f1020f73b238aac7fd9f0546e7ec5b553b3b
Pibo Shongwe <piboman@yahoo.com>
2024-01-29 13:02:55 -0800

    u

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - insert new node left
+ * @parent: pointer to the node to be created
+ * @value: node data
+ * Return: a pointer to the created node, or
+ * NULL on failure or if parent is NULL
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+binary_tree_t *new_node;
+
+if (parent == NULL)
+	return (NULL);
+
+new_node = binary_tree_node(parent, value);
+if (new_node == NULL)
+	return (NULL);
+
+if (parent->right != NULL)
+{
+new_node->right = parent->right;
+parent->right->parent = new_node;
+}
+parent->right = new_node;
+return (new_node);
+}
