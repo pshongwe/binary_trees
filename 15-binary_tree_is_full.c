43f308ccb8c06aaf686098e14b7ede274084e4a6
Pibo Shongwe <piboman@yahoo.com>
2024-01-31 08:08:05 -0800

    u

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,11 +9,12 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (1);
-
-	if ((tree->left && !tree->right) || (!tree->left && tree->right))
 		return (0);
-
+	else if (!tree->left && !tree->right)
+		return (1);
+	else if ((tree->left && tree->right))
 	return (binary_tree_is_full(tree->left) && binary_tree_is_full
 			(tree->right));
+	else
+		return (0);
 }
