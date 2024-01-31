267b2806c71838b4f9fd573dc7901b20ac20055e
Pibo Shongwe <piboman@yahoo.com>
2024-02-01 00:29:58 +0200

    Update 100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,6 +1,24 @@
 #include "binary_trees.h"
 
 /**
+ * depth - Measures the depth of a
+ * node in a binary tree
+ * @node: Pointer to the node to measure the depth
+ * Return: The depth of the node
+ */
+size_t depth(const binary_tree_t *node)
+{
+size_t d = 0;
+
+while (node != NULL)
+{
+d++;
+node = node->parent;
+}
+return (d);
+}
+
+/**
  * binary_trees_ancestor - Finds the lowest common
  * ancestor of two nodes
  * @first: Pointer to the first node
