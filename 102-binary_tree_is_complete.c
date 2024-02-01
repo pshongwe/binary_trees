f19abc6f1fbd986f93bf2a3c7d0a14717e5d83bf
Darknught <hassino66@yahoo.com>
2024-02-01 18:07:53 +0000

    code

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -11,7 +11,6 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
 	const binary_tree_t **queue, *current, **temp;
-	int null_seen = 0;
 	size_t front = 0, rear = 0, queue_size = 1;
 
 	queue = malloc(1 * sizeof(binary_tree_t *));
@@ -30,21 +29,11 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	{
 		current = queue[front++];
 
-		if (null_seen && current != NULL)
+		if (current != NULL)
 		{
-			free(queue);
-			return (0);
-		}
-
-		if (current == NULL)
-		{
-			null_seen = 1;
-		}
-		else
-		{
-			if (current->left != NULL || current->right != NULL)
+			if (current->left != NULL)
 			{
-				if (rear + 2 > queue_size)
+				if (rear + 1 >= queue_size)
 				{
 					temp = realloc(queue, (queue_size * 2) * sizeof(binary_tree_t *));
 
@@ -57,6 +46,22 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 					queue_size *= 2;
 				}
 				queue[rear++] = current->left;
+			}
+
+			if (current->right != NULL)
+			{
+				if (rear + 1 >= queue_size)
+				{
+					temp = realloc(queue, (queue_size * 2) * sizeof(binary_tree_t *));
+
+					if (temp == NULL)
+					{
+						free(queue);
+						return (0);
+					}
+					queue = temp;
+					queue_size *= 2;
+				}
 				queue[rear++] = current->right;
 			}
 		}
